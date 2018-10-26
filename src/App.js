48f922efef11fad7017855a47244c9db12ae00e2
Subham Bhattacharjee <subham.bhattacharjee1@gmail.com>
2018-10-26 16:41:34 +0530

    some what working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import { Button } from 'reactstrap';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
 import Title from "./components/Title";
 import Coindata from "./components/Coindata";
+import Footer from "./components/Footer";
 
 const GLOBAL_COIN_API = 'https://api.coinmarketcap.com/v2/global/';
 const COIN_API = 'https://api.coinmarketcap.com/v2/ticker/?limit=10';
+const SINGLE_COIN_API = 'https://api.coinmarketcap.com/v2/ticker/';
 
 class App extends React.Component {
 
@@ -21,8 +23,41 @@ class App extends React.Component {
             total_market_cap: undefined,
             total_volume_24h: undefined
           },
-          coins: []
+          coins: [],
+          modal: false,
+          coin_data: {
+            name: undefined,
+            market_cap: undefined,
+            price: undefined,
+            volume_24h: undefined,
+            circulating_supply: undefined,
+            percent_change_24h: undefined
+          }
         }
+        this.toggle = this.toggle.bind(this);
+    }
+
+    toggle() {
+        this.setState({
+          modal: !this.state.modal
+        });
+    }
+
+    single_coin_data(coin_id) {
+        fetch(SINGLE_COIN_API + coin_id + '/')
+          .then(response => response.json())
+          .then(json => {
+            this.setState({
+                coin_data: {
+                    name: json.data.name,
+                    market_cap: json.data.quotes.USD.market_cap,
+                    price: json.data.quotes.USD.price,
+                    volume_24h: json.data.quotes.USD.volume_24h,
+                    circulating_supply: json.data.circulating_supply,
+                    percent_change_24h: json.data.quotes.USD.percent_change_24h
+                }
+            });
+        });
     }
 
     componentDidMount() {
@@ -52,7 +87,7 @@ class App extends React.Component {
 
 
     render() {
-        console.log(this.state.coins)
+        console.log(this.state.coin_data)
         var arr = [];
         for (var key in this.state.coins) {
           arr.push(this.state.coins[key]);
@@ -74,23 +109,20 @@ class App extends React.Component {
               </div>
             </nav>
             <div>
-
                 <table className="table">
                   <thead>
                     <tr>
-                      <th scope="col">Name</th>
-                      <th scope="col">Market Cap</th>
-                      <th scope="col">Price</th>
-                      <th scope="col">Volume(24h)</th>
-                      <th scope="col">Ciculating Supply</th>
-                      <th scope="col">Change(24h)</th>
+                      <th>Name</th>
+                      <th>Market Cap</th>
+                      <th>Price</th>
+                      <th>Volume(24h)</th>
+                      <th>Ciculating Supply</th>
+                      <th>Change(24h)</th>
                     </tr>
                   </thead>
                   <tbody>
-                    
-
                     {arr.map(item => (
-                        <tr key={item.id}>
+                        <tr key={item.id} onClick={(event) => { this.toggle(); this.single_coin_data(item.id);}}>
                             <td>{item.name}</td>
                             <td>{item.quotes.USD.market_cap}</td>
                             <td>${item.quotes.USD.price}</td>
@@ -102,6 +134,21 @@ class App extends React.Component {
                   </tbody>
                 </table>
             </div>
+            <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+              <ModalHeader toggle={this.toggle}>{ this.state.coin_data.name }</ModalHeader>
+              <ModalBody>
+                <b>{this.state.coin_data.name}</b> <br/>
+                <b>Market Cap:</b>{this.state.coin_data.market_cap} <br/>
+                <b>Price:</b> {this.state.coin_data.price} <br/>
+                <b>Volume:</b> {this.state.coin_data.volume_24h} <br/>
+                <b>Circulating Supply:</b> {this.state.coin_data.circulating_supply} <br/>
+                <b>Percent Change 24h:</b> {this.state.coin_data.percent_change_24h}
+              </ModalBody>
+              <ModalFooter>
+                <Button color="secondary" onClick={this.toggle}>OK</Button>
+              </ModalFooter>
+            </Modal>
+            <Footer/>
           </div>
         );
     }
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+const Footer = props => (
+	<div>
+		<p>Made with <span style={{color: '#e74c3c'}}>&hearts;</span> by Subham Bhattacharjee</p>
+	</div>
+
+);
+
+export default Footer;
